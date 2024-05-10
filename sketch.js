6ee82649156a8ef715962e6d9c78e49c9ea76db8
unknown <doctorklum@gmail.com>
2024-05-09 17:24:57 -0800

    working responsive grid

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-let d = 2;
+let d = 1;
 let i, j, n;
 
 let mic, fft;
 
-let band = 64;
+let band = 32;
 
-let unit = 8;
+let unit = 5;
 let count;
 let modA = [];
 
@@ -15,19 +15,19 @@ function setup() {
 
   mic = new p5.AudioIn();
   mic.start();
-  fft = new p5.FFT(0.1, band);
+  fft = new p5.FFT(0.9, band);
   fft.setInput(mic);
 
   modA = new Module(unit, unit);
 
-  let wideCount = band / 16;
-  let highCount = band / 16;
+  let wideCount = band / 4;
+  let highCount = band / 4;
   count = wideCount * highCount;
 
   let index = 0;
   for (let y = 0; y < highCount; y++) {
     for (let x = 0; x < wideCount; x++) {
-      modA[index++] = new Module(unit * x, unit * y, 4, 4, unit);
+      modA[index++] = new Module(unit * x, unit * y, 4, 4, unit, y);
     }
   }
 }
@@ -46,7 +46,7 @@ function keyTyped() {
 }
 
 class Module {
-  constructor(xOff, yOff, xsz, ysz, unit) {
+  constructor(xOff, yOff, xsz, ysz, unit, band) {
     this.xOff = xOff;
     this.yOff = yOff;
     this.xsz = xsz;
@@ -55,14 +55,16 @@ class Module {
     this.band;
     this.lrp;
     this.unit = unit;
+    this.band = band;
   }
 
   // Custom method for updating the variables
   updateDraw() {
     orbitControl();
     let spectrum = fft.analyze();
+
     for (i = 0; i < band; i++) {
-      this.zsz = map(spectrum[18], 0, 255, 0, height);
+      this.zsz = map(spectrum[this.band], 0, 2000, 0, height);
       this.lrp = lerp(0, this.zsz, 0.1);
 
       push();
